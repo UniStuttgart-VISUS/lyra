a70cfcf1dfec5d70eb3b316e9a07f7bc4768b6ab
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2026-05-11 18:51:12 +0200

    Added the (empty) getter for the consolidated data.

---
+++ autodoc/include/visus/autodoc/consolidated.h
@@ -0,0 +1,38 @@
+﻿// <copyright file="consolidated.h" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2026 Visualisierungsinstitut der Universität Stuttgart.
+// Licensed under the MIT licence. See LICENCE file for details.
+// </copyright>
+// <author>Christoph Müller</author>
+
+#if !defined(_LYRA_CONSOLIDATED_H)
+#define _LYRA_CONSOLIDATED_H
+#pragma once
+
+#include "visus/autodoc/raw.h"
+
+
+LYRA_NAMESPACE_BEGIN
+
+namespace consolidated {
+
+    /// <summary>
+    /// Identifies a property holding the timestamp when the information was
+    /// collected.
+    /// </summary>
+    typedef raw::timestamp timestamp;
+
+    /// <summary>
+    /// Gets the consolidated system information, which is the same as the raw
+    /// data after removing duplicates. This function tries to find the most
+    /// data about the system it can, but removes the noise from the raw data.
+    /// </summary>
+    /// <param name="flags">Allows for customising the collection
+    /// behaviour.</param>
+    LYRA_API property_set get(_In_ const collection_flags flags
+        = collection_flags::none);
+
+} /* namespace consolidated */
+
+LYRA_NAMESPACE_END
+
+#endif /* !defined(_LYRA_CONSOLIDATED_H) */
---
+++ autodoc/src/consolidated.cpp
@@ -0,0 +1,40 @@
+﻿// <copyright file="consolidated.cpp" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2026 Visualisierungsinstitut der Universität Stuttgart.
+// Licensed under the MIT licence. See LICENCE file for details.
+// </copyright>
+// <author>Christoph Müller</author>
+
+#include "visus/autodoc/consolidated.h"
+
+#include "visus/autodoc/application.h"
+#include "visus/autodoc/cpu.h"
+#include "visus/autodoc/environment.h"
+#include "visus/autodoc/graphics.h"
+#include "visus/autodoc/hardware.h"
+#include "visus/autodoc/memory.h"
+#include "visus/autodoc/network.h"
+#include "visus/autodoc/mounts.h"
+#include "visus/autodoc/operating_system.h"
+#include "visus/autodoc/smbios.h"
+#include "visus/autodoc/timestamp.h"
+
+#include "property_set_impl.h"
+
+
+/*
+ * LYRA_NAMESPACE::consolidated::get
+ */
+LYRA_NAMESPACE::property_set LYRA_NAMESPACE::consolidated::get(
+        _In_ const collection_flags flags) {
+    detail::property_set_impl ps;
+
+    // First, add the timestamp.
+    if (detail::check_sensitive<timestamp>(flags)) {
+        const auto now = LYRA_NAMESPACE::timestamp::now();
+        ps.add<timestamp>(now);
+    }
+
+    throw "TODO";
+
+    return property_set(std::move(ps));
+}
